93bec42cce01cfbe14155f5d212a3a8dff34a761
yawkat <me@yawk.at>
2017-04-24 11:50:07 +0200

    Refactor io loop to exit the daemon on graphitesend failure

--- daemon.py
+++ daemon.py
@@ -3,7 +3,7 @@
 import re
 import socket
 import traceback
-from typing import Dict
+from typing import Dict, Optional
 
 import graphitesend
 
@@ -18,6 +18,8 @@ last_data_by_uid: Dict[str, Dict] = {}
 
 while True:
     buffer: bytes = recv_socket.recv(4096)
+
+    to_send: Optional[Dict] = None
     # noinspection PyBroadException
     try:
         decoded = buffer.decode("ascii")
@@ -27,28 +29,29 @@ while True:
             if "xAP-hbeat" in data:
                 uid = data["xAP-hbeat"]["uid"]
                 # resend data as a "heartbeat" to keep this machine alive in grafana
-                graphitesend.send_dict(last_data_by_uid[uid])
+                to_send = last_data_by_uid[uid]
             else:
                 print("xAP-header not present in %s" % (decoded,))
-            continue
-
-        uid = data["xAP-header"]["uid"]
-        source = data["xAP-header"]["source"]
-        source_match = re.fullmatch(r"Almico\.SpeedFan\.(.+)", source)
-        if source_match:
-            dev_id = source_match.group(1)
-            prefix = dev_id + '.speedfan.'
-            collected_data = {}
-            for sensor_id, values in data.items():
-                if "curr" in values:
-                    sensor_name = values["id"]
-                    collected_data[prefix + sensor_id + "." + sensor_name] = values["curr"]
-            if len(collected_data) > 0:
-                graphitesend.send_dict(collected_data)
-                last_data_by_uid[uid] = collected_data
-            else:
-                print("Collected no data from xAP %s" % (decoded,))
         else:
-            print("Received xAP from source %s, discarding" % (source,))
+            uid = data["xAP-header"]["uid"]
+            source = data["xAP-header"]["source"]
+            source_match = re.fullmatch(r"Almico\.SpeedFan\.(.+)", source)
+            if source_match:
+                dev_id = source_match.group(1)
+                prefix = dev_id + '.speedfan.'
+                collected_data = {}
+                for sensor_id, values in data.items():
+                    if "curr" in values:
+                        sensor_name = values["id"]
+                        collected_data[prefix + sensor_id + "." + sensor_name] = values["curr"]
+                if len(collected_data) > 0:
+                    to_send = collected_data
+                    last_data_by_uid[uid] = collected_data
+                else:
+                    print("Collected no data from xAP %s" % (decoded,))
+            else:
+                print("Received xAP from source %s, discarding" % (source,))
     except:
         traceback.print_exc()
+    if to_send is not None:
+        graphitesend.send_dict(to_send)
